f8cc24b7713fe7fad011725c44867fd876c508e6
Iano Maciel <ianomaciel6385@gmail.com>
2021-10-23 21:18:05 -0400

    App.tsx: adicionado o menu drawer no componente App

--- App.tsx
+++ App.tsx
@@ -1,21 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+/*View and component*/
+import { HomeScreen } from './src/view/HomeScreen';
+import { Profile } from './src/view/Profile';
+import { CustomDrawer } from './src/component/CustomDrawer';
+import { PatientForm } from './src/view/PatientForm';
+
+
+const Drawer = createDrawerNavigator();
+
+export function MyDrawer()
+{
+  return(
+    <Drawer.Navigator 
+      screenOptions={{
+        drawerStyle: {
+          backgroundColor: '#448893',
+          //width: 240,
+        },
+        drawerActiveBackgroundColor: '#fff',
+        drawerInactiveTintColor: '#fff',
+        drawerActiveTintColor: '#000',
+        headerStyle: {
+          backgroundColor: '#448893', //Set Header color
+        },
+        headerTintColor: '#fff', //Set Header text color
+      }}
+      initialRouteName="Home"
+      drawerContent={(props) => <CustomDrawer {...props}/>}
+    >
+      <Drawer.Screen name="Home" component={HomeScreen} />
+      <Drawer.Screen name="Perfil" component={Profile} />
+      <Drawer.Screen name="Formulário do Paciente" component={PatientForm}/>
+    </Drawer.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyDrawer/>
+    </NavigationContainer>
+  );
+}
