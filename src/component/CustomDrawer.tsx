befabd941ae565271cf7c30bba920abf4e99b318
Iano Maciel <ianomaciel6385@gmail.com>
2021-10-23 21:15:47 -0400

    (#12): Desenvolvimento do menu Drawer Navigator

---
+++ src/component/CustomDrawer.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { View, Text, Image, StyleSheet  } from 'react-native';
+import { DrawerContentScrollView, DrawerItemList } from '@react-navigation/drawer'
+
+export function CustomDrawer(props)
+{
+    return(
+        <View style={{flex:1}}> 
+            <View style={styles.areaProfile}>
+                <Image
+                    source={ require('../img/profile.png') }
+                    style={styles.pictureProfile}
+                />
+                <Text style={{fontWeight: 'bold', fontSize: 19, color: '#000'}}>Full name</Text>
+            </View>
+            <DrawerContentScrollView {...props}>
+                <DrawerItemList {...props} />
+            </DrawerContentScrollView>
+
+            <View style={styles.logoDrawer}>
+                <Image
+                    source={ require('../img/Logo-png.png') }
+                    style={{width: 60, height: 60}}
+                />
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    pictureProfile: {
+        width: 100,
+        height: 100,
+        borderRadius: 50,
+        marginTop: 30
+    },
+    areaProfile: {
+        backgroundColor: '#fff',
+        padding: 10,
+        marginBottom: 10,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    logoDrawer: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 200
+    }
+});
