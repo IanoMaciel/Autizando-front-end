ff881a7575c8129cbaa8d380d3584ef0a3151b86
Iano Maciel <ianomaciel6385@gmail.com>
2021-11-04 15:00:48 -0400

    Updated

--- App.tsx
+++ App.tsx
@@ -7,6 +7,12 @@ import { HomeScreen } from './src/view/HomeScreen';
 import { Profile } from './src/view/Profile';
 import { CustomDrawer } from './src/component/CustomDrawer';
 import { PatientForm } from './src/view/PatientForm';
+import { Login } from './src/view/Login';
+import { Logout } from './src/view/Logout';
+import { RecorverPass1 } from './src/view/RecorverPass1';
+import { RecorverPass2 } from './src/view/RecoverPass2';
+import { RegistrationScreen } from './src/view/RegistrationScreen';
+import { FindExpertScreen } from './src/view/FindExpertScreen';
 
 
 const Drawer = createDrawerNavigator();
@@ -33,7 +39,13 @@ export function MyDrawer()
     >
       <Drawer.Screen name="Home" component={HomeScreen} />
       <Drawer.Screen name="Perfil" component={Profile} />
+      <Drawer.Screen name="Cadastro" component={RegistrationScreen} />
+      <Drawer.Screen name="Login" component={Login} />
       <Drawer.Screen name="Formulário do Paciente" component={PatientForm}/>
+      <Drawer.Screen name="Encontrar Especialista" component={FindExpertScreen}/>
+      <Drawer.Screen name="Recuperar Senha" component={RecorverPass1}/>
+      <Drawer.Screen name="Senha" component={RecorverPass2}/>
+      <Drawer.Screen name="Sair" component={Logout}/>
     </Drawer.Navigator>
   );
 }
